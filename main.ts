e0e9dae5c0dc23c243ddc20637b63afe03e7fd4e
Manuel Ávila Rodríguez <mastercraft9000@gmail.com>
2021-06-18 23:55:10 -0500

    Se agrego un texto que muestra un resumen de la historia

--- main.ts
+++ main.ts
@@ -1,19 +1,26 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    controller.moveSprite(mySprite)
+    controller.moveSprite(myPlayer)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    playerProjectile.vy += -10
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    controller.moveSprite(mySprite)
+    controller.moveSprite(myPlayer)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    controller.moveSprite(mySprite)
+    controller.moveSprite(myPlayer)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    controller.moveSprite(mySprite)
+    controller.moveSprite(myPlayer)
 })
-let mySprite: Sprite = null
+let myPlayer: Sprite = null
+let playerProjectile: Sprite = null
 scene.setBackgroundImage(assets.image`game_bg`)
-mySprite = sprites.create(assets.image`Player`, SpriteKind.Player)
-let main_enemy = sprites.create(assets.image`enemy`, SpriteKind.Player)
+game.showLongText("Una raza de extraterrestres con forma reptiliana ha secuestrado a Sophie", DialogLayout.Bottom)
+playerProjectile = sprites.create(assets.image`player_rojectile`, SpriteKind.Projectile)
+myPlayer = sprites.create(assets.image`Player`, SpriteKind.Player)
+let myEnemy = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
+myEnemy.follow(myPlayer)
 while (true) {
     music.playMelody("E B C5 A B G A F ", 60)
 }
